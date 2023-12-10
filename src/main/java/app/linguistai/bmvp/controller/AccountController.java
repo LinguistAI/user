d935b49ec0e5f0a3938daa9891a1c8a2180fc736
Ilkim Elif Kervan <elif.kervan.1907@gmail.com>
2023-12-10 18:47:04 +0300

    chore(LINGUIST-125): Removed unnecessary imports

--- src/main/java/app/linguistai/bmvp/controller/AccountController.java
+++ src/main/java/app/linguistai/bmvp/controller/AccountController.java
@@ -11,7 +11,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
--- src/main/java/app/linguistai/bmvp/controller/gamification/UserStreakController.java
+++ src/main/java/app/linguistai/bmvp/controller/gamification/UserStreakController.java
@@ -2,7 +2,6 @@ package app.linguistai.bmvp.controller.gamification;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
