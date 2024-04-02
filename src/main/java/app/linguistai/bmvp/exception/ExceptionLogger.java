6cc1fb7ac6df7bc278adf6534131475fdfc954d1
Yağız Can Aslan <86242974+can-aslan@users.noreply.github.com>
2024-04-02 12:17:35 +0300

    fix(LINGUIST-312): Fixed UserStreak Not Updating Due to Null Assignment Error on User Login (#37)
    * fix(LINGUIST-312): Fixed UserStreak Not Updating Due to Null Assignment Error on User Login
    
    * fix(LINGUIST-312): Improved performance of UserStreakService.incrementUserStreak() and addressed PR comments

--- src/main/java/app/linguistai/bmvp/exception/ExceptionLogger.java
+++ src/main/java/app/linguistai/bmvp/exception/ExceptionLogger.java
@@ -5,7 +5,8 @@ import lombok.Data;
 @Data
 public class ExceptionLogger {
     private static final boolean NEXT_LINE_FLAG = false; 
-    
+
+    @Deprecated
     public static String log(Exception e) {
         String msg = "";
         msg += (e.getMessage().equals(e.getLocalizedMessage())) ? e.getLocalizedMessage()
@@ -14,6 +15,7 @@ public class ExceptionLogger {
         return msg;
     }
 
+    @Deprecated
     public static String warn(Exception e) {
         String msg = "[WARNING] ";
         msg += (e.getMessage().equals(e.getLocalizedMessage())) ? e.getLocalizedMessage()
@@ -22,6 +24,7 @@ public class ExceptionLogger {
         return msg;
     }
 
+    @Deprecated
     public static String error(Exception e) {
         String msg = "[ERROR] ";
         msg += (e.getMessage().equals(e.getLocalizedMessage())) ? e.getLocalizedMessage()
