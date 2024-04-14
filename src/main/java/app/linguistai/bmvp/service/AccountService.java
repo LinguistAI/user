ab9fa562acf86eb76d839740f153279bc24dc448
Kardelen Ceren <kceren199@gmail.com>
2024-04-14 14:31:21 +0300

    feat(LINGUIST-355): User login checks merged (#46)
    * feat(LINGUIST-355): User login checks merged
    
    * fix(LINGUIST-355): Addressed PR review

--- src/main/java/app/linguistai/bmvp/service/AccountService.java
+++ src/main/java/app/linguistai/bmvp/service/AccountService.java
@@ -6,13 +6,11 @@ import java.util.UUID;
 
 import app.linguistai.bmvp.exception.AlreadyFoundException;
 import app.linguistai.bmvp.exception.CustomException;
-import app.linguistai.bmvp.exception.ExceptionLogger;
 import app.linguistai.bmvp.exception.LoginException;
 import app.linguistai.bmvp.exception.NotFoundException;
 import app.linguistai.bmvp.exception.PasswordNotMatchException;
 import app.linguistai.bmvp.exception.SomethingWentWrongException;
 import app.linguistai.bmvp.exception.StreakException;
-import app.linguistai.bmvp.exception.TokenException;
 import app.linguistai.bmvp.model.ResetToken;
 import app.linguistai.bmvp.repository.IResetTokenRepository;
 import app.linguistai.bmvp.service.currency.ITransactionService;
@@ -77,25 +75,12 @@ public class AccountService {
             final String accessToken = jwtUtils.createAccessToken(userDetails);
             final String refreshToken = jwtUtils.createRefreshToken(userDetails);
 
-            // If login is successful, check whether to increase user streak or not
-            // ALSO IN MESSAGE SERVICE: userStreakService.updateUserStreak(dbUser.getEmail());
-            try {
-                userStreakService.updateUserStreak(dbUser.getEmail());
-            }
-            catch (Exception e1) {
-                // Intentionally not thrown to not cause login exception for users without UserStreak
-                System.out.println(ExceptionLogger.log(e1));
-            }
+            this.initiateUserSession(dbUser.getEmail());
 
             log.info("User {} logged in.", dbUser.getId());
-            // Add the current date as a logged date
-            userLoggedDateService.addLoggedDateByEmailAndDate(dbUser.getEmail(), new Date());
-            transactionService.ensureUserGemsExists(dbUser.getEmail());
-
             return new RLoginUser(dbUser, accessToken, refreshToken);
         } catch (CustomException e2) {
             log.error("User login failed due to wrong email or password for email {}", user.getEmail());
-
             throw e2;
         } catch (Exception e2) {
             log.error("User login failed for email {}", user.getEmail(), e2);
@@ -103,6 +88,17 @@ public class AccountService {
         }
     }
 
+    public void loginWithValidToken(String email) throws Exception {
+        // This method is only reached if the user already has a valid token
+        try {
+            this.initiateUserSession(email);
+            log.info("User with email {} logged in with a valid token.", email);
+        } catch (Exception e) {
+            log.error("User login with valid token failed for email {}", email, e);
+            throw new SomethingWentWrongException();
+        }
+    }
+  
     public Page<User> searchUser(QUserSearch userSearch, String userEmail) throws Exception {
         try {
             // Create a page request using the request body
@@ -312,5 +308,20 @@ public class AccountService {
             log.error("Set password failed for email {}", email, e);
             throw new SomethingWentWrongException();
         }
+    }
+
+    private void initiateUserSession(String email) throws Exception {
+        // Upon successful user entry, check whether to increase user streak or not
+        try {
+            userStreakService.updateUserStreak(email);
+        }
+        catch (Exception e) {
+            // Intentionally not thrown to not cause login exception for users without UserStreak
+            log.error("User streak update is failed for email {} when initiating user session.", email, e);
+        }
+        // Add the current date as a logged date
+        userLoggedDateService.addLoggedDateByEmailAndDate(email, new Date());
+        transactionService.ensureUserGemsExists(email);
+        log.info("User session initiated for email {}.", email);
     }
 }
