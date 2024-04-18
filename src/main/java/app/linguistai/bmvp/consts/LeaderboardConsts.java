e27e22837fad47fc1dff3b80cedd14bce2070297
Kardelen Ceren <kceren199@gmail.com>
2024-04-18 10:18:32 +0300

    fix(LINGUIST-393): Leaderboard secondly ordered by username (#56)

--- src/main/java/app/linguistai/bmvp/consts/LeaderboardConsts.java
+++ src/main/java/app/linguistai/bmvp/consts/LeaderboardConsts.java
@@ -2,4 +2,5 @@ package app.linguistai.bmvp.consts;
 
 public class LeaderboardConsts {
     public static final String XP_SORT_FIELD = "experience";
+    public static final String USERNAME_SORT_FIELD = "user.username";
 }
