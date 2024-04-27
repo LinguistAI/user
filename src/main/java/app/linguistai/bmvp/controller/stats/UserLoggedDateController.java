343cd038ece4385c7a0cfc1480bb63d98916543e
Tolga Özgün <tolgaozgunn@gmail.com>
2024-04-27 12:03:38 +0300

    Add user logged date support for other users

--- src/main/java/app/linguistai/bmvp/controller/stats/UserLoggedDateController.java
+++ src/main/java/app/linguistai/bmvp/controller/stats/UserLoggedDateController.java
@@ -10,10 +10,13 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @AllArgsConstructor
 @RestController
@@ -22,7 +25,7 @@ public class UserLoggedDateController {
 
     private final UserLoggedDateService loggedDateService;
 
-    @Operation(summary = "Get Logged Date", description = "Retrieve logged dates for the user.")
+    @Operation(summary = "Get Logged Date", description = "Retrieve logged dates for this user.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Successfully fetched logged dates", content =
                     {@Content(mediaType = "application/json", schema =
@@ -36,6 +39,25 @@ public class UserLoggedDateController {
                                                 @RequestParam(required = false, defaultValue = "desc") String sort,
                                                 @RequestParam(required = false) Integer daysLimit) throws Exception {
         RUserLoggedDate loggedDates = loggedDateService.getLoggedDates(email, sort, daysLimit);
+        return Response.create("Successfully fetched logged dates", HttpStatus.OK, loggedDates);
+    }
+
+
+    @Operation(summary = "Get Logged Date", description = "Retrieve logged dates for the user.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully fetched logged dates", content =
+                    {@Content(mediaType = "application/json", schema =
+                    @Schema(implementation = RUserLoggedDate.class))}),
+            @ApiResponse(responseCode = "400", description = "Invalid input parameter"),
+            @ApiResponse(responseCode = "404", description = "User not found"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
+    @GetMapping("/logged-date/{userId}")
+    public ResponseEntity<Object> getLoggedDateAnotherUser(@RequestHeader(Header.USER_EMAIL) String email,
+                                                           @Valid @PathVariable("userId") UUID userId,
+                                                @RequestParam(required = false, defaultValue = "desc") String sort,
+                                                @RequestParam(required = false) Integer daysLimit) throws Exception {
+        RUserLoggedDate loggedDates = loggedDateService.getLoggedDates(userId, sort, daysLimit);
         return Response.create("Successfully fetched logged dates", HttpStatus.OK, loggedDates);
     }
 }
