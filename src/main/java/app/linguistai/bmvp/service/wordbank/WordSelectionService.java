64319d60590890a83148a1619a86c53449a3f439
Tolga Özgün <tolgaozgunn@gmail.com>
2024-04-28 15:58:45 +0300

    Cover edge case where user has no/insufficient active lists/words in WordSelectionService.java

--- src/main/java/app/linguistai/bmvp/service/wordbank/WordSelectionService.java
+++ src/main/java/app/linguistai/bmvp/service/wordbank/WordSelectionService.java
@@ -9,6 +9,7 @@ import app.linguistai.bmvp.exception.NotFoundException;
 import app.linguistai.bmvp.exception.SomethingWentWrongException;
 import app.linguistai.bmvp.repository.wordbank.IUnknownWordRepository;
 import app.linguistai.bmvp.repository.wordbank.IWordSelectionRepository;
+import app.linguistai.bmvp.request.wordbank.QPredefinedWordList;
 import org.springframework.stereotype.Service;
 
 import app.linguistai.bmvp.model.User;
@@ -20,6 +21,9 @@ import app.linguistai.bmvp.request.QSelectWord;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.transaction.annotation.Transactional;
+
+import static app.linguistai.bmvp.consts.FilePaths.DEFAULT_WORD_LIST_FILE;
+import static app.linguistai.bmvp.utils.FileUtils.readPredefinedWordListFromYamlFile;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -120,10 +124,31 @@ public class WordSelectionService {
             selectedWords.addAll(unknownWordRepository.findRandomByOwnerListUserIdAndOwnerListIsActiveAndConfidence(
                 userId, true, confidences, selectSize));
 
+            if (selectedWords.size() == initialSelectSize) {
+                return selectedWords;
+            }
+
+            log.error("Error in selecting new words, selected words size is not equal to initial select size, " +
+                    "selectedWords size: {}, initialSelectSize: {}", selectedWords.size(), initialSelectSize);
+
+            QPredefinedWordList predefinedWordList = readPredefinedWordListFromYamlFile(DEFAULT_WORD_LIST_FILE);
+
+            predefinedWordList.getWords().forEach(word -> {
+                if (selectedWords.size() < initialSelectSize) {
+                    UnknownWord unknownWord = UnknownWord.builder()
+                            .word(word)
+                            .confidence(ConfidenceEnum.LOWEST)
+                            .ownerList(null)
+                            .build();
+
+                    selectedWords.add(unknownWord);
+                }
+            });
+
             if (selectedWords.size() != initialSelectSize) {
                 log.error("Error in selecting new words, selected words size is not equal to initial select size, " +
                         "selectedWords size: {}, initialSelectSize: {}", selectedWords.size(), initialSelectSize);
-                throw new NotFoundException("Error in selecting new words, you need to add more unknown words to your word bank");
+                throw new SomethingWentWrongException("Error in selecting new words, you need to add more unknown words to your word bank");
             }
 
             return selectedWords;
