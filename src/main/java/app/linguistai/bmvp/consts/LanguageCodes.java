0a1bc57dbe583e71d9c112ea77ab420f1b5edfbf
Yagiz Can Aslan <can.aslan@ug.bilkent.edu.tr>
2024-05-16 00:44:32 +0300

    feat(LINGUIST-595): Added codes for other languages.

--- src/main/java/app/linguistai/bmvp/consts/LanguageCodes.java
+++ src/main/java/app/linguistai/bmvp/consts/LanguageCodes.java
@@ -8,6 +8,9 @@ public class LanguageCodes {
     public static final String CODE_TURKISH = "TUR";
     public static final String CODE_SPANISH = "ESP";
     public static final String CODE_ITALIAN = "ITA";
+    public static final String CODE_GERMAN = "GER";
+    public static final String CODE_FRENCH = "FRA";
+    public static final String CODE_KOREAN = "KOR";
 
-    public static final List<String> ALL_CODES = Arrays.asList(CODE_ENGLISH, CODE_TURKISH, CODE_SPANISH, CODE_ITALIAN);
+    public static final List<String> ALL_CODES = Arrays.asList(CODE_ENGLISH, CODE_TURKISH, CODE_SPANISH, CODE_ITALIAN, CODE_GERMAN, CODE_FRENCH, CODE_KOREAN);
 }
